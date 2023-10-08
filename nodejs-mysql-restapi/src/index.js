a0ddf6812d5ac1c18c1969df22242dede8182d47
Niel <danielopez225@gmail.com>
2023-10-08 16:37:28 -0600

    Variables de entorno

--- nodejs-mysql-restapi/src/index.js
+++ nodejs-mysql-restapi/src/index.js
@@ -1,26 +1,5 @@
-import express from 'express'
-import preguntasRoutes from './routes/preguntas.routes.js'
-import indexRoutes from './routes/index.routes.js'
+import app from './app.js'
+import { PORT } from './config.js'
 
-const app = express()
-
-app.use(express.json())
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Esto permite todas las solicitudes, puedes restringirlo a un dominio específico en producción.
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
-
-app.use(indexRoutes)
-app.use( '/api', preguntasRoutes)
-
-app.use((req, res, next) => {
-    res.status(404).json({
-        message: 'endpoint no encontrada'
-    })
-})
-
-
-app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'))
+app.listen(PORT);
+console.log('Servidor corriendo en puerto', PORT)
